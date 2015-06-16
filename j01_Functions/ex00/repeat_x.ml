42ce56c57ec91c6245ecfd4c39925ab5380be3d2
Nicolas GOGUEY <ngoguey@e1r6p19.42.fr>
2015-06-16 09:40:19 +0200

    ex00

---
+++ j01_Functions/ex00/repeat_x.ml
@@ -0,0 +1,36 @@
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                        :::      ::::::::   *)
+(*   repeat_x.ml                                        :+:      :+:    :+:   *)
+(*                                                    +:+ +:+         +:+     *)
+(*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        *)
+(*                                                +#+#+#+#+#+   +#+           *)
+(*   Created: 2015/06/16 09:13:54 by ngoguey           #+#    #+#             *)
+(*   Updated: 2015/06/16 09:19:22 by ngoguey          ###   ########.fr       *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+let repeat_x n =
+
+  let rec loop s n =
+	if n <= 0 then
+	  s
+	else
+	  loop (s ^ "x") (n - 1)
+  in
+
+  if n < 0 then
+	"error"
+  else
+	loop "" n
+
+		 
+let test n =
+  Printf.printf "Test with [%+2d]: %s\n%!" n (repeat_x n)
+
+let () =
+  test (-1);
+  test 0;
+  test 1;
+  test 2;
+  test 3;
