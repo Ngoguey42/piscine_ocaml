9baf5c55d8b76ec57916391dc7f1fafc5380d630
Nicolas GOGUEY <ngoguey@e1r6p19.42.fr>
2015-06-16 17:38:18 +0200

    fixed palin

--- j00_OCaml_basics/ex07/ft_is_palindrome.ml
+++ j00_OCaml_basics/ex07/ft_is_palindrome.ml
@@ -6,7 +6,7 @@
 (*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/06/15 11:27:13 by ngoguey           #+#    #+#             *)
-(*   Updated: 2015/06/15 15:23:55 by ngoguey          ###   ########.fr       *)
+(*   Updated: 2015/06/16 17:37:58 by ngoguey          ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -16,7 +16,7 @@ let rec loop s i j =
 	  if String.get s i <> String.get s j then
 		false
 	  else
-		loop s (i + 1) (i - 1)
+		loop s (i + 1) (j - 1)
 	end
   else
 	true
@@ -33,4 +33,9 @@ let () =
   test "ab";
   test "aba";
   test "abab";
-  test "ababa"
+  test "ababa";
+  test "aa";
+  test "abba";
+  test "abbab";
+  test "abbaba";
+  test "abbabba";
