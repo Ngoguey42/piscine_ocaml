7288ebb0af517d443df789470f3d1631c20c9669
Nicolas GOGUEY <ngoguey@e1r5p20.42.fr>
2015-06-18 19:37:21 +0200

    fixed ex01 d02

--- j02_PatternMatching_Types/ex01/crossover.ml
+++ j02_PatternMatching_Types/ex01/crossover.ml
@@ -6,7 +6,7 @@
 (*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/06/17 15:18:45 by ngoguey           #+#    #+#             *)
-(*   Updated: 2015/06/17 16:06:10 by ngoguey          ###   ########.fr       *)
+(*   Updated: 2015/06/18 19:36:43 by ngoguey          ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -20,7 +20,7 @@ let crossover la lb =
   let rec loop l la lb =
 	match la with
 	| []											-> l
-	| hd::tl when not (is_in l hd) && is_in lb hd	-> loop (hd::l) tl lb
+	| hd::tl when is_in lb hd						-> loop (hd::l) tl lb
 	| _::tl											-> loop l tl lb
   in
   loop [] la lb
