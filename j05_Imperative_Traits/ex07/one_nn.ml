de48197b7a4c695db549932d8a564033a5a00135
Nicolas GOGUEY <ngoguey@e1r5p20.42.fr>
2015-06-22 15:21:26 +0200

    done ex07

--- j05_Imperative_Traits/ex07/one_nn.ml
+++ j05_Imperative_Traits/ex07/one_nn.ml
@@ -6,7 +6,7 @@
 (*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/06/22 13:09:26 by ngoguey           #+#    #+#             *)
-(*   Updated: 2015/06/22 14:50:35 by ngoguey          ###   ########.fr       *)
+(*   Updated: 2015/06/22 15:19:54 by ngoguey          ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -27,14 +27,14 @@ let eu_dist a b =
 
 let rad_eu_list (a, _) (b, _q) =
   eu_dist a b
-		  
+
 let one_nn (l: radar list) (r: radar) =
   let rec helper l closestdiff closest =
 	match l with
 	| []								-> (closestdiff, closest)
 	| hd::tl
 	  -> let diff = rad_eu_list r hd in
-		 if diff < 0. || diff < closestdiff then
+		 if closestdiff < 0. || diff < closestdiff then
 		   helper tl diff hd
 		 else
 		   helper tl closestdiff closest
@@ -42,7 +42,6 @@ let one_nn (l: radar list) (r: radar) =
   let (diff, closest) = helper l (-.1.) ([||], "") in
   let (_, cl) = closest in
   cl
-	
 
 let is_float_char = function
   | "-" | "+" | "." | "0" | "1" | "2" | "3"
@@ -75,7 +74,7 @@ let parse_line fchan =
 	with Failure "EOL"	-> ()
   end;
   (Array.of_list !l, !classstr)
-	
+
 let examples_of_files fname =
   try
 	let fchan = open_in fname in
@@ -102,12 +101,41 @@ let test fname r =
   let l = examples_of_files fname in
   let n = List.length l in
   Printf.printf "List length: %d\n%!" n;
+  let cl = one_nn l r in
+  Printf.printf "radar class: \"%s\" \n%!" cl;
   Printf.printf "\n%!"
-				
 
-				
-				
 let () =
-  test "ionosphere.test.csv";
-  test "ionosphere.train.csv";
-
+  let r1 = ([|1.;0.;0.84710;0.13533;0.73638;-.0.06151;
+			  0.87873;0.08260;0.88928;-.0.09139;0.78735;
+			  0.06678;0.80668;-.0.00351;0.79262;-.0.01054;
+			  0.85764;-.0.04569;0.87170;-.0.03515;0.81722;
+			  -.0.09490;0.71002;0.04394;0.86467;-.0.15114
+			  ;0.81147;-.0.04822;0.78207;-.0.00703;0.75747;
+			  -.0.06678;0.85764;-.0.06151|], "lol")
+  in
+  let r2 = ([|1.;0.;0.94331;0.19959;0.96132;
+			  0.40803;0.80514;0.56569;0.56687;
+			  0.70830;0.41836;0.83230;0.14939;
+			  0.89489;0.05167;0.93682;-.0.24742;
+			  0.83939;-.0.42811;0.75554;-.0.50251;
+			  0.62563;-.0.65515;0.50428;-.0.68851;
+			  0.30912;-.0.77097;0.15619;-.0.75406;
+			  -.0.04399;-.0.75199;-.0.17921;-.0.66932;
+			  -.0.34367|], "lol2")
+  in
+  let r3 = ([|1.;1263150.;0.94331;0.19959;0.96132;
+			  0.40803;0.80514;0.56569;0.56687;
+			  0.70830;0.41836;0.83230;0.14939;
+			  0.89489;0.05167;0.93682;-.0.24742;
+			  0.83939;-.0.42811;0.75554;-.0.50251;
+			  0.62563;-.0.65515;0.50428;-.0.68851;
+			  0.30912;-.0.77097;0.15619;-.0.75406;
+			  -.0.04399;-.0.75199;-.0.17921;-.0.66932;
+			  -.0.34367|], "lol2")
+  in
+  test "ionosphere.test.csv" r1;
+  test "ionosphere.test.csv" r2;
+  test "ionosphere.test.csv" r3;
+  test "ionosphere.train.csv" r1;
+  test "ionosphere.train.csv" r2;
