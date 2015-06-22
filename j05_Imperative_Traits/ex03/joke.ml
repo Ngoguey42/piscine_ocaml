60838f16e4e7c6be160832dfe09f754ba0470697
Nicolas GOGUEY <ngoguey@e1r5p20.42.fr>
2015-06-22 12:00:40 +0200

    done ex03

---
+++ j05_Imperative_Traits/ex03/joke.ml
@@ -0,0 +1,55 @@
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                        :::      ::::::::   *)
+(*   joke.ml                                            :+:      :+:    :+:   *)
+(*                                                    +:+ +:+         +:+     *)
+(*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        *)
+(*                                                +#+#+#+#+#+   +#+           *)
+(*   Created: 2015/06/22 11:40:08 by ngoguey           #+#    #+#             *)
+(*   Updated: 2015/06/22 12:00:22 by ngoguey          ###   ########.fr       *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+let parse_file fchan =
+  let l = ref [] in
+  begin
+	try
+	  while true do
+		let line = input_line fchan in
+		l := line::(!l);
+	  done;
+	with
+	| End_of_file	-> ();
+  end;
+  Array.of_list !l
+
+let () =
+  try
+	begin
+	  let fname = Sys.argv.(1) in
+	  let fchan = open_in fname in
+	  let jokes = parse_file fchan in
+	  let n = Array.length jokes in
+	  if n <= 0 then
+		failwith "no joke found :(";
+	  Random.self_init ();
+	  print_endline (jokes.(Random.int n))
+	end
+  with
+  | Failure "no joke found :("
+	-> print_endline ("Catched : no joke found in file !!")
+  | Invalid_argument m
+	-> print_endline ("Catched \"" ^ m ^ "\" please give some arguments")
+  | Sys_error m
+	-> print_endline ("Catched \"" ^ m ^ "\" oh no !!")
+
+					 (*
+ocamlopt joke.ml && ./a.out
+ocamlopt joke.ml && ./a.out ""
+ocamlopt joke.ml && ./a.out "/exam"
+ocamlopt joke.ml && ./a.out "lul"
+ocamlopt joke.ml && ./a.out "."
+ocamlopt joke.ml && ./a.out empty.joke
+ocamlopt joke.ml && ./a.out full.joke
+ocamlopt joke.ml && ./a.out one.joke
+					  *)
