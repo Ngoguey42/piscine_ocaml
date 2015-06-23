2a72912ec71e6af056d1beca035742dcecec014a
Nicolas GOGUEY <ngoguey@e1r5p20.42.fr>
2015-06-23 18:50:50 +0200

    changed test main

--- j05_Imperative_Traits/ex08/k_nn.ml
+++ j05_Imperative_Traits/ex08/k_nn.ml
@@ -6,7 +6,7 @@
 (*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/06/22 15:36:40 by ngoguey           #+#    #+#             *)
-(*   Updated: 2015/06/23 18:36:50 by ngoguey          ###   ########.fr       *)
+(*   Updated: 2015/06/23 18:50:29 by ngoguey          ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -20,7 +20,7 @@ let eu_dist a b =
 	failwith "Arrays are empty !!";
   let square (acc, i) av =
 	let bv = b.(i) in
-	((av -. bv) ** 2. -. acc, i + 1)
+	((av -. bv) ** 2. +. acc, i + 1)
   in
   let (squaredsum, _) = Array.fold_left square (0., 0) a in
   sqrt squaredsum
@@ -71,99 +71,37 @@ let k_nn (l: radar list) k (r: radar) =
   let ar = Array.sub ar 0 k in
   main_occurence ar k
 				 
-let is_float_char = function
-  | "-" | "+" | "." | "0" | "1" | "2" | "3"
-  | "4" | "5" | "6" | "7" | "8" | "9"	-> true
-  | _ 									-> false
-
-(* Ugliest code i've ever written, for real *)
-let parse_line fchan =
-  let l = ref [] in
-  let nbrstr = ref "" in
-  let classstr = ref "" in
-  begin
-	try
-	  while true do
-		let c = really_input_string fchan 1 in
-		if c = "\n" then
-		  raise (Failure "EOL")
-		else if c = " " then
-		  ()
-		else if c = "," then
-		  begin
-			l := !l @ [float_of_string !nbrstr];
-			nbrstr := ""
-		  end
-		else if is_float_char c then
-		  nbrstr := !nbrstr ^ c
-		else
-		  classstr := !classstr ^ c
-	  done
-	with Failure "EOL"	-> ()
-  end;
-  (Array.of_list !l, !classstr)
-
-let examples_of_files fname =
-  try
-	let fchan = open_in fname in
-	let l = ref [] in
-	begin
-	  try
-		while true do
-		  l := !l @ [parse_line fchan]
-		done;
-	  with
-	  | End_of_file	-> ();
-	end;
-	!l
-  with
-  | Failure "float_of_string"
-	-> print_endline ("Catched \"float_of_string\" csv file corrupted !!"); []
-  | Invalid_argument m
-	-> print_endline ("Catched \"" ^ m ^ "\" please give some arguments"); []
-  | Sys_error m
-	-> print_endline ("Catched \"" ^ m ^ "\" oh no !!"); []
-
-let test fname r =
-  Printf.printf "Test with %s:\n%!" fname;
-  let l = examples_of_files fname in
-  let n = List.length l in
-  Printf.printf "List length: %d\n%!" n;
-  let cl = k_nn l 3 r in
-  Printf.printf "radar class: \"%s\" \n%!" cl;
-  Printf.printf "\n%!"
 
 let () =
-  let r1 = ([|1.;0.;0.84710;0.13533;0.73638;-.0.06151;
-			  0.87873;0.08260;0.88928;-.0.09139;0.78735;
-			  0.06678;0.80668;-.0.00351;0.79262;-.0.01054;
-			  0.85764;-.0.04569;0.87170;-.0.03515;0.81722;
-			  -.0.09490;0.71002;0.04394;0.86467;-.0.15114
-			  ;0.81147;-.0.04822;0.78207;-.0.00703;0.75747;
-			  -.0.06678;0.85764;-.0.06151|], "lol")
-  in
-  let r2 = ([|1.;0.;0.94331;0.19959;0.96132;
-			  0.40803;0.80514;0.56569;0.56687;
-			  0.70830;0.41836;0.83230;0.14939;
-			  0.89489;0.05167;0.93682;-.0.24742;
-			  0.83939;-.0.42811;0.75554;-.0.50251;
-			  0.62563;-.0.65515;0.50428;-.0.68851;
-			  0.30912;-.0.77097;0.15619;-.0.75406;
-			  -.0.04399;-.0.75199;-.0.17921;-.0.66932;
-			  -.0.34367|], "lol2")
-  in
-  let r3 = ([|1.;1263150.;0.94331;0.19959;0.96132;
-			  0.40803;0.80514;0.56569;0.56687;
-			  0.70830;0.41836;0.83230;0.14939;
-			  0.89489;0.05167;0.93682;-.0.24742;
-			  0.83939;-.0.42811;0.75554;-.0.50251;
-			  0.62563;-.0.65515;0.50428;-.0.68851;
-			  0.30912;-.0.77097;0.15619;-.0.75406;
-			  -.0.04399;-.0.75199;-.0.17921;-.0.66932;
-			  -.0.34367|], "lol2")
-  in
-  test "ionosphere.test.csv" r1;
-  test "ionosphere.test.csv" r2;
-  test "ionosphere.test.csv" r3;
-  test "ionosphere.train.csv" r1;
-  test "ionosphere.train.csv" r2;
+  let radars = [
+	  ([|-0.67321; 0.80893; -0.40446; 0.06264; 0.97763; 0.04474; 0.95973|], "a");
+	  ([|0.66667; -0.01366; 0.97404; 0.06831; 0.49590; 0.50137; 0.75683|], "b");
+	  ([|0.66667; -0.01366; 0.97404; 0.06831; 0.49590; 0.50137; 0.75683|], "b");
+	  ([|0.83609; 0.13215; 0.72171; 0.06059; 0.65829; 0.08315; 0.23888|], "c");
+	  ([|0.12961; 0.43837; 0.20330; 0.49418; 0.12686; 0.44747; 0.13507|], "c");
+	  ([|0.88085; 0.35232; 0.68389; 0.65128; 0.34816; 0.79784; 0.05832|], "b");
+	  ([|0.90842; -0.29784; 0.86490; -0.62635; 0.69590; 0.79378; 0.29492|], "c");
+	  ([|0.87111; 0.04326; 0.79946; 0.18297; 0.99009; 0.29292; 0.49590|], "a");
+	  ([|0.86889; -0.07111; 1.0000; -0.02494; 1.0000; -0.06889; 0.1|], "y");
+	] in
+  print_string "Nearest class of test0 is (k = 4): ";
+  print_endline (k_nn radars 4
+					  ([|0.02337; -0.00592; -0.09924; -0.11949; -0.00763; -0.11824; 0.14706|], "test0"));
+  print_string "Nearest radar of test0 is: ";
+  print_endline (k_nn radars 1
+						([|0.02337; -0.00592; -0.09924; -0.11949; -0.00763; -0.11824; 0.14706|], "test0"));
+  print_string "Nearest class of test1 is (k = 4): ";
+  print_endline (k_nn radars 4
+					  ([|0.74852; -0.02811; 0.65680; -0.05178; 0.80621; 0.02811; 0.|], "test1"));
+  print_string "Nearest radar of test1 is: ";
+  print_endline (k_nn radars 1
+						([|0.74852; -0.02811; 0.65680; -0.05178; 0.80621; 0.02811; 0.|], "test1"));
+  print_string "Nearest class of test2 is (k = 4): ";
+  print_endline (k_nn radars 4
+					  ([|0.66667; -0.01366; 0.97404; 0.06831; 0.49590; 0.50137; 0.75683|], "test2"));
+  print_string "Nearest radar of test2 is: ";
+  print_endline (k_nn radars 1
+						([|0.66667; -0.01366; 0.97404; 0.06831; 0.49590; 0.50137; 0.75683|], "test2"))
+
+
+				
