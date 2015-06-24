8d3d06111771c43e1bf5c9d9071268b89f86d5c9
Nicolas GOGUEY <ngoguey@e1r5p20.42.fr>
2015-06-24 16:48:00 +0200

    done ex02

--- j07_OOP_1/ex02/main.ml
+++ j07_OOP_1/ex02/main.ml
@@ -6,7 +6,7 @@
 (*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/06/24 13:54:14 by ngoguey           #+#    #+#             *)
-(*   Updated: 2015/06/24 16:47:37 by ngoguey          ###   ########.fr       *)
+(*   Updated: 2015/06/24 16:46:50 by ngoguey          ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -26,6 +26,17 @@ let () =
   Printf.printf "d1 = Travel in time 42 84\n%!";
   let d1 = d1#travel_in_time 42 84 in
   Printf.printf "to_string: %s\n%!" (d1#to_string);
-  d1#use_sonic_screwdriver;
-  d1#use_sonic_screwdriver;
   Printf.printf "Ma fonction regenerate fonctionne, jte le jure\n%!";
+  Printf.printf "Test: ex02\n%!";
+  let dal1 = new Dalek.dalek in
+  Printf.printf "to_string: %s\n%!" (dal1#to_string);
+  Printf.printf "Talk:\n%!"; dal1#talk;
+  Printf.printf "Talk:\n%!"; dal1#talk;
+  Printf.printf "Talk:\n%!"; dal1#talk;
+  Printf.printf "Exterminate:\n%!"; dal1#exterminate p1;Printf.printf "to_string: %s\n%!" (dal1#to_string); d1#use_sonic_screwdriver;
+  Printf.printf "Exterminate:\n%!"; dal1#exterminate p1;Printf.printf "to_string: %s\n%!" (dal1#to_string); d1#use_sonic_screwdriver;
+  Printf.printf "Exterminate:\n%!"; dal1#exterminate p1;Printf.printf "to_string: %s\n%!" (dal1#to_string); d1#use_sonic_screwdriver;
+  Printf.printf "Die:\n%!";
+  dal1#die;
+  Printf.printf "\n%!";
+  
