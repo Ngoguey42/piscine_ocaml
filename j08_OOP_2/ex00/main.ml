982e872be0ebccada6f59593c06e744cdf2b6c7a
Nicolas GOGUEY <ngoguey@e1r5p20.42.fr>
2015-06-25 12:06:35 +0200

    added ex00

---
+++ j08_OOP_2/ex00/main.ml
@@ -0,0 +1,30 @@
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                        :::      ::::::::   *)
+(*   main.ml                                            :+:      :+:    :+:   *)
+(*                                                    +:+ +:+         +:+     *)
+(*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        *)
+(*                                                +#+#+#+#+#+   +#+           *)
+(*   Created: 2015/06/25 12:01:59 by ngoguey           #+#    #+#             *)
+(*   Updated: 2015/06/25 12:06:17 by ngoguey          ###   ########.fr       *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+let () =
+  Printf.printf "\027[34m%s\027[0m\n%!" "Tests ex00:";
+  Printf.printf "New Atom:%! to_string: %s\n%!" (new Atom.hydrogen)#to_string;
+  Printf.printf "New Atom:%! to_string: %s\n%!" (new Atom.helium)#to_string;
+  Printf.printf "New Atom:%! to_string: %s\n%!" (new Atom.carbon)#to_string;
+  Printf.printf "New Atom:%! to_string: %s\n%!" (new Atom.nitrogen)#to_string;
+  Printf.printf "New Atom:%! to_string: %s\n%!" (new Atom.oxygen)#to_string;
+  Printf.printf "New Atom:%! to_string: %s\n%!" (new Atom.fluorine)#to_string;
+  Printf.printf "New Atom:%! to_string: %s\n%!" (new Atom.sodium)#to_string;
+  Printf.printf "New Atom:%! to_string: %s\n%!" (new Atom.magnesium)#to_string;
+  Printf.printf "New Atom:%! to_string: %s\n%!" (new Atom.aluminum)#to_string;
+  Printf.printf "New Atom:%! to_string: %s\n%!" (new Atom.silicon)#to_string;
+  Printf.printf "New Atom:%! to_string: %s\n%!" (new Atom.phosphorus)#to_string;
+  Printf.printf "New Atom:%! to_string: %s\n%!" (new Atom.sulfur)#to_string;
+  Printf.printf "New Atom:%! to_string: %s\n%!" (new Atom.chlorine)#to_string;
+  Printf.printf "New Atom:%! to_string: %s\n%!" (new Atom.argon)#to_string;
+  Printf.printf "New Atom:%! to_string: %s\n%!" (new Atom.potassium)#to_string;
+  Printf.printf "New Atom:%! to_string: %s\n%!" (new Atom.calcium)#to_string;
