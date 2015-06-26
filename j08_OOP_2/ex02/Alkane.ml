e08e7e40cfae16a0143f23a4f40454261f5df908
Nicolas GOGUEY <ngoguey@e1r5p20.42.fr>
2015-06-26 19:37:45 +0200

    fixed ex02

--- j08_OOP_2/ex02/Alkane.ml
+++ j08_OOP_2/ex02/Alkane.ml
@@ -6,9 +6,30 @@
 (*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/06/25 14:29:07 by ngoguey           #+#    #+#             *)
-(*   Updated: 2015/06/25 14:55:24 by ngoguey          ###   ########.fr       *)
+(*   Updated: 2015/06/26 19:37:11 by ngoguey          ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
+
+let name_of_int = function
+  | 1-> "Methane"
+  | 2-> "Ethane"
+  | 3-> "Propane"
+  | 4-> "Butane"
+  | 5-> "Pentane"
+  | 6-> "Hexane"
+  | 7-> "Heptane"
+  | 8-> "Octane"
+  | 9-> "Nonane"
+  | 10-> "Decane"
+  | 11-> "Undecane"
+  | 12-> "Dodecane"
+  | 16-> "Hexadecane"
+  | 20-> "Icosane"
+  | 30-> "Triacontane"
+  | 40-> "Tetracontane"
+  | 50-> "Pentacontane"
+  | 60-> "Hexacontane"
+  | _-> "Alkane"
 
 let list_of_n n =
   let rec helper elt n acc =
@@ -21,7 +42,9 @@ let list_of_n n =
   else
 	(helper (new Atom.carbon) n []) @ (helper (new Atom.hydrogen) (2 * n + 2) [])
 
-class alkane n = object inherit Molecule.molecule "alkane" (list_of_n n) end
+class alkane n = object
+  inherit Molecule.molecule (name_of_int n) (list_of_n n)
+end
 class methane = object inherit Molecule.molecule "methane" (list_of_n 1) end
 class ethane = object inherit Molecule.molecule "ethane" (list_of_n 2) end
 class propane = object inherit Molecule.molecule "propane" (list_of_n 3) end
